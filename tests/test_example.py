07d09dd902c2f0222c97a53bb1113fc999769408
Andrey <andrei_chesanov@mail.ru>
2024-10-06 20:46:00 +0300

    add new test with files

--- tests/test_example.py
+++ tests/test_example.py
@@ -7,3 +7,15 @@ def test_reverse():
 
 def test_reverse_for_empty_string():
     assert reverser('') == ''
+
+
+def test_reverse_with_files():
+    files = 'in.txt'
+    with open(files, 'r') as f, open('out.txt', 'r+')as a:
+        value = f.read()
+        a.write(reverser(value))
+        a.seek(0)
+        read_value = a.read()
+        assert read_value == "telxeH"
+
+
